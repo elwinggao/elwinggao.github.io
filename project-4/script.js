816122a3fa6c101f4cd7c094d2f71b3224b04a34
elwinggao <elwing.gao@cooper.edu>
2025-05-05 16:52:08 -0400

    050525 update??

--- project-4/script.js
+++ project-4/script.js
@@ -7,6 +7,7 @@ let puerhRaw = document.getElementById("puerhRaw");
 let puerhRipe = document.getElementById("puerhRipe");
 
 let teaType = document.getElementById("teaType");
+let timer = document.getElementById("safeTimerDisplay");
 
 let pot = document.getElementById("pot");
 let temperature = document.getElementById("temperature");
@@ -18,56 +19,56 @@ const dropdown = document.getElementById('teaType');
 
 dropdown.addEventListener('change', function () {
     if (green.selected == true){
-        tea.style.color = "green";
+        timer.style.color = "green";
         pot.innerHTML = "gaiwan, glass cup";
         temperature.innerHTML = "95°C";
         quantity.innerHTML = "3-4 grams";
         ratio.innerHTML = "Water to Tea: 1:50";
     }
     else if (white.selected == true){
-        tea.style.color = "white";
+        timer.style.color = "white";
         pot.innerHTML = "gaiwan";
         temperature.innerHTML = ">95°C";
         quantity.innerHTML = "5 grams";
         ratio.innerHTML = "Water to Tea: 1:30";
     }
     else if (yellow.selected == true){
-        tea.style.color = "yellow";
+        timer.style.color = "yellow";
         pot.innerHTML = "gaiwan, glass";
         temperature.innerHTML = "95°C";
         quantity.innerHTML = "3-4 grams";
         ratio.innerHTML = "Water to Tea: 1:50";
     }
     else if (black.selected == true){
-        tea.style.color = "black";
+        timer.style.color = "black";
         pot.innerHTML = "gaiwan, piaoyi cup/glass pot";
         temperature.innerHTML = ">95°C";
         quantity.innerHTML = "4 grams";
         ratio.innerHTML = "Water to Tea: 1:40";
     }
     else if (puerhRaw.selected == true){
-        tea.style.color = "brown";
+        timer.style.color = "brown";
         pot.innerHTML = "gaiwan, clay teapot";
         temperature.innerHTML = ">100°C";
         quantity.innerHTML = "8 grams";
         ratio.innerHTML = "Water to Tea: 1:20";
     }
     else if (puerhRipe.selected == true){
-        tea.style.color = "red";
+        timer.style.color = "red";
         pot.innerHTML = "gaiwan, clay teapot";
         temperature.innerHTML = ">100°C";
         quantity.innerHTML = "6 grams";
         ratio.innerHTML = "Water to Tea: 1:20";
     }
     else if (floral.selected == true){
-        tea.style.color = "pink";
+        timer.style.color = "pink";
         pot.innerHTML = "gaiwan";
         temperature.innerHTML = "95°C";
         quantity.innerHTML = "4 grams";
         ratio.innerHTML = "Water to Tea: 1:40";
     }
     else {
-        tea.style.color = "grey";
+        timer.style.color = "grey";
     }
 });
 
@@ -91,8 +92,49 @@ function teaTimer(duration, display) {
 
 function startTimer() {
     if (green.selected == true) { 
-    var time = 10, // your time in seconds here
+    var time = 10,
         display = document.querySelector('#safeTimerDisplay');
     teaTimer(time, display);
     }
+    else if (yellow.selected == true) { 
+        var time = 30,
+            display = document.querySelector('#safeTimerDisplay');
+        teaTimer(time, display);
+        }
 };
+
+
+function updateSeason() {
+
+    let today = new Date();
+    let thisMonth = today.getMonth();
+    // let thisHour = today.getHours();
+    // let thisMinute = today.getMinutes();
+    // let thisSecond = today.getSeconds();
+    
+    // let hours = document.getElementById("hour");
+    // let minutes = document.getElementById("minutes");
+    // let seconds = document.getElementById("seconds");   
+      
+    console.log(thisMonth);
+
+    if (thisMonth >= 2 && thisMonth <= 4) {
+        document.body.style.backgroundImage = "url('images/spring.jpg')";
+        document.body.style.backgroundSize = "cover";
+    }
+    else if (thisMonth >= 5 && thisMonth <= 7) {
+        document.body.style.backgroundImage = "url('images/summer.jpeg')";
+        document.body.style.backgroundSize = "cover";
+    }
+    else if (thisMonth >= 8 && thisMonth <= 11) {
+        document.body.style.backgroundImage = "url('images/fall.jpg')";
+        document.body.style.backgroundSize = "cover";
+    }
+    else if (thisMonth >= 12 && thisMonth <= 2) {
+        document.body.style.backgroundImage = "url('images/winter.jpg')";
+        document.body.style.backgroundSize = "cover";
+    }
+    };
+    
+    document.body.onload = updateSeason();
+    // setInterval(updateSeason, 1000);
